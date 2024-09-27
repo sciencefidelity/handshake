465ea9337e1dd218edb311334dd90aa5502ec7ba
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-09-27 09:26:55 +0100

    ci: add nix build command

--- flake.nix
+++ flake.nix
@@ -14,22 +14,36 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+        manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+        name = manifest.name;
+        version = manifest.version;
       in
       with pkgs;
       {
+        packages.default = pkgs.rustPlatform.buildRustPackage
+          {
+            pname = name;
+            version = version;
+            cargoLock.lockFile = ./Cargo.lock;
+            src = pkgs.lib.cleanSource ./.;
+          };
+
         devShells.default = mkShell {
           buildInputs = [
             cargo-feature
+            cargo-machete
+            cargo-tarpaulin
             pkg-config
             taplo
             (rust-bin.stable.latest.default.override {
               extensions = [ "rust-analyzer" "rust-src" ];
             })
           ];
+        };
 
-          shellHook = /*bash*/ ''
-
-          '';
+        apps.${name} = {
+          type = "app";
+          program = "${self.packages.${system}.runme}/bin/${name}";
         };
       }
     );
